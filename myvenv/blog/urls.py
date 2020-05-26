e8cb2e93d855c1a276b533dc3d3f520ba8a75d24
KaanAksoy93 <64798668+KaanAksoy93@users.noreply.github.com>
2020-05-26 20:30:19 +0200

    Update urls.py

--- myvenv/blog/urls.py
+++ myvenv/blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from .views import (PostListView,PostDetailView)
+
 
 urlpatterns = [
-    path('',views.home, name='blog-home'),
-    path('about/',views.about, name='blog-about'),
-    path('contact/',views.contact, name='blog-contact'),
-    path('<int:pk>', views.PostDetailView, name='post-detail'),
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+    # path('about/', PostListView.as_view(), name='blog-about'),
+
+    path('contact/', views.contact, name='blog-contact'),
+    # path('<int:pk>', views.PostDetailView, name='post-detail'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
