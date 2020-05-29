c58818ac0831ef334ae4a2267e37e7fb2124c6c4
KaanAksoy93 <64798668+KaanAksoy93@users.noreply.github.com>
2020-05-29 13:31:39 +0200

    Added_NewPost/Update/Delete by Kaan

--- myvenv/blog/views.py
+++ myvenv/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
 from .models import Post
 from django.core.mail import send_mail
-from django.views.generic import (DetailView,ListView)
+from django.views.generic import (DetailView,ListView,CreateView,UpdateView,DeleteView)
 
 
 
@@ -21,6 +22,39 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template = 'post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+		model = Post
+		fields = ['title', 'content']
+
+		def form_valid(self, form):
+			form.instance.author = self.request.user
+			return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
