7650b3d61c87adda9091e6a319e3c2bf4f08acfc
KaanAksoy93 <64798668+KaanAksoy93@users.noreply.github.com>
2020-05-29 13:32:52 +0200

    Added_NewPost/Update/Delete

--- myvenv/blog/urls.py
+++ myvenv/blog/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from . import views
-from .views import (PostListView,PostDetailView)
+from .views import (PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView)
 
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
+
     path('about/', views.about, name='blog-about'),
     # path('about/', PostListView.as_view(), name='blog-about'),
 
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
     path('contact/', views.contact, name='blog-contact'),
     # path('<int:pk>', views.PostDetailView, name='post-detail'),
+
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
