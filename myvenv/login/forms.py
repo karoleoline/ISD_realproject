d6396eb3065e2b86d7258fe322d5d033814422f9
karoline <karoline.leo@uni.li>
2020-05-31 09:52:52 +0200

    misc.
    added password change
    new profile url

--- myvenv/login/forms.py
+++ myvenv/login/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import UserProfile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -8,14 +9,14 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = (
+        fields = [
             'username',
             'first_name',
             'last_name',
             'email',
             'password1',
             'password2',
-        )
+        ]
 
     def save(self, commit=True):
         user = super(UserRegisterForm, self).save(commit=False)
@@ -28,14 +29,31 @@ class UserRegisterForm(UserCreationForm):
 
         return user
 
-
+"""
 class EditProfileForm(UserChangeForm):
 
     class Meta:
         model = User
         fields = (
+            'email',
+            'first_name',
+            'last_name',
+        )
+"""
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = [
             'username',
             'first_name',
             'last_name',
             'email',
-        )
+            ]
+
+class UserProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['image']
--- myvenv/login/urls.py
+++ myvenv/login/urls.py
@@ -8,19 +8,10 @@ from django.contrib.auth.views import (PasswordResetView, PasswordResetDoneView,
 
 urlpatterns = [
     path('register/',views.register, name='register'),
-    path('profile/',views.profile, name='profile'),
+    url(r'^profile/(?P<username>[\w\-]+)/$', views.profile, name='profile'),
+    #path('profile/',views.profile, name='profile'),
     #path('profile/account/', views.edit_profile, name='editprofile'),
     path('profile/password/',PasswordResetView.as_view(), name='change-profile'),
-    url(r'^profile/(?P<pk>\d+)/$', views.view_profile, name='view_profile_with_pk'),
-    url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
-    url(r'^change-password/$', views.change_password, name='change_password'),
 
-    url(r'^reset-password/$', PasswordResetView, {'template_name': 'accounts/reset_password.html', 'post_reset_redirect': 'accounts:password_reset_done', 'email_template_name': 'accounts/reset_password_email.html'}, name='reset_password'),
-
-    url(r'^reset-password/done/$', PasswordResetDoneView, {'template_name': 'accounts/reset_password_done.html'}, name='password_reset_done'),
-
-    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', PasswordResetConfirmView, {'template_name': 'accounts/reset_password_confirm.html', 'post_reset_redirect': 'accounts:password_reset_complete'}, name='password_reset_confirm'),
-
-    url(r'^reset-password/complete/$', PasswordResetCompleteView,{'template_name': 'accounts/reset_password_complete.html'}, name='password_reset_complete')
 
 ]
