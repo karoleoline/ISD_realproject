43ec7e2f2d517452d6945aeefb02f0925a00aff4
karoline <karoline.leo@uni.li>
2020-06-01 19:16:57 +0200

    updated profile

--- myvenv/login/urls.py
+++ myvenv/login/urls.py
@@ -8,9 +8,10 @@ from django.contrib.auth.views import (PasswordResetView, PasswordResetDoneView,
 
 urlpatterns = [
     path('register/',views.register, name='register'),
+    #path('profile/',views.profile, name='profile'),
     url(r'^profile/(?P<username>[\w\-]+)/$', views.profile, name='profile'),
     #path('profile/',views.profile, name='profile'),
-    #path('profile/account/', views.edit_profile, name='editprofile'),
+    path('profile/account/', views.edit_profile, name='editprofile'),
     path('profile/password/',PasswordResetView.as_view(), name='change-profile'),
 
 
